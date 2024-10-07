7b74d2838e1ed32f64c240fbb2430d934d82db9b
Leonardo Brum <leonardobrum.dev@gmail.com>
2024-10-07 08:38:22 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+const Volunteers = mongoose.model('Volunteers', {
+      name: String,
+      email: String,
+      phone: String,
+      address: String,
+      area: String,
+      availability: String,
+      comment: String
+})
+
+app.get("/", async (req, res) => {
+      const volunteers = await Volunteers.find()
+      return res.send(volunteers)
+})
+
+app.delete("/:id",  async (req, res) => {
+      const volunteer = await Volunteers.findByIdAndDelete(req.params.id);
+      return res.send(volunteer)
+})
+
+app.put("/:id",  async (req, res) => {
+      const volunteer = await Volunteers.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            address: req.body.address,
+            area: req.body.area,
+            availability: req.body.availability,
+            comment: req.body.comment
+      }, {
+            new: true
+      });
+      return res.send(volunteer)
+})
+
+app.post("/", async (req, res) => {
+      const volunteer = new Volunteers({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            address: req.body.address,
+            area: req.body.area,
+            availability: req.body.availability,
+            comment: req.body.comment
+      })
+
+      await volunteer.save()
+      return res.send(volunteer)
+})
+
+app.listen(port, () => {
+      mongoose
+            .connect(process.env.ATLAS_URI)
+            .then(() => console.log("BD conectado"))
+            .catch(err => console.log(err));
+      console.log(`Está rodando na porta: ${port}`)
+})
